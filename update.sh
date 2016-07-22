af87c311cc141d2ca24256d829eea1c10e816973
Denton Liu <liu.denton+github@gmail.com>
2016-07-22 15:26:28 -0400

    Add git-completion.bash to download list

--- update.sh
+++ update.sh
@@ -2,3 +2,4 @@
 
 mkdir -p bashrc_sources
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o bashrc_sources/git-prompt.sh 
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o bashrc_sources/git-completion.bash
