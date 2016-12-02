7dc80fbf8c9680dbbeaffdc97e8b4929531f6ada
Denton Liu <liu.denton+github@gmail.com>
2016-12-02 01:38:01 -0500

    Change incorrect update.sh source

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@
 mkdir -p bashrc_sources
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o bashrc_sources/git-prompt.sh 
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o bashrc_sources/git-completion.bash
-curl https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/tmux.completion.bash -o bashrc_sources/git-completion.bash
+curl https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/tmux.completion.bash -o bashrc_sources/tmux.completion.bash
